fb52062b1ea4bf56b3468b8e5becbfa43e41da9c
Riley Eaton <ryeaton@student.ubc.ca>
2024-09-22 02:46:30 -0700

    improve shell visuals

--- src/shell.c
+++ src/shell.c
@@ -6,6 +6,8 @@
 
 #define MAX_COMMAND_LENGTH 1024
 #define MAX_ARGS 100
+#define MAX_PATH 4096
+#define MAX_USERID 500
 
 // Function to parse the command and its arguments
 void parse_command(char* command, char** args) {
@@ -21,25 +23,36 @@ void parse_command(char* command, char** args) {
 void execute_command(char** args) {
     pid_t pid = fork(); // Create a child process
     if (pid == 0) { // Child process
-        if (execvp(args[0], args) == -1) { // Execute the command
-            perror("shell");
-        }
+        execvp(args[0], args); // Try to execute the command
+        // If execvp returns, an error occurred
+        perror(args[0]); // Print the command name with the error
         exit(EXIT_FAILURE);
     } else if (pid < 0) { // Error forking
-        perror("shell");
+        perror("fork");
     } else { // Parent process
         int status;
         waitpid(pid, &status, 0); // Wait for the child process to complete
     }
 }
 
+
 int main() {
+    // Store the command ran, arguments, the current working directory, and the user's username
     char command[MAX_COMMAND_LENGTH];
     char* args[MAX_ARGS];
+    char cwd[MAX_PATH];
+    char username[MAX_USERID];
+    cuserid(username);
 
     while (1) {
+        // Store working directory and print error on failure
+        if (getcwd(cwd, sizeof(cwd)) == NULL) {
+            perror("getcwd() error");
+            return -1;
+        }
+
         // Print shell prompt
-        printf("myshell> ");
+        printf("%s IntelliShell ~%s$ ", username, cwd); // Every run, simulate a shell's initial text
         fflush(stdout);
 
         // Read command input
@@ -59,6 +72,18 @@ int main() {
         }
         if (strcmp(args[0], "exit") == 0) {
             break; // Exit the shell
+        }
+
+        // Handle the cd command in the parent process (changing directories in child process doesnt work)
+        if (strcmp(args[0], "cd") == 0) {
+            if (args[1] == NULL) {
+                fprintf(stderr, "cd: expected argument to \"cd\"\n");
+            } else {
+                if (chdir(args[1]) != 0) {
+                    perror("cd");
+                }
+            }
+            continue; // Skip the rest and start the next iteration
         }
 
         // Execute the command
